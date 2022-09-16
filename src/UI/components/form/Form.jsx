080c528812587de08224f2597e5cb4ec2bc4fe47
Your Name <you@example.com>
2022-09-16 09:29:52 -0300

    creando carpeta assests

--- src/UI/components/form/Form.jsx
+++ src/UI/components/form/Form.jsx
@@ -1,10 +1,29 @@
+import { useForm } from '../../../hooks/index'; 
 import './form.css'
 
 export const Form = () => {
+
+  const { searchInfo,onInputChange,reset } = useForm({
+    searchInfo:'',
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    reset();
+  }
+  
   return (
-    <form>
-      <input type="text" className="form-control" placeholder="search film..." autoComplete="off" />
-      <button className="btn btn-outline-info">Search</button>
+    <form onSubmit={ onSubmit }>
+      <input 
+        type="text"
+        className="form-control"
+        placeholder="search film..."
+        autoComplete="off"
+        name='searchInfo' 
+        value={ searchInfo } 
+        onChange={ onInputChange } 
+        />
+      <button className="btn btn-outline-info form-btn">Search</button>
     </form>
   )
 }
