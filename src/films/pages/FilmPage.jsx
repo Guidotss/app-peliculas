e7d44d64e59e128b85c89b30af33878ec0e994bf
Your Name <you@example.com>
2022-09-19 16:50:08 -0300

    terminando film page

--- src/films/pages/FilmPage.jsx
+++ src/films/pages/FilmPage.jsx
@@ -3,6 +3,7 @@ import { getFilmByTitle } from '../index';
 import './filmPage.css'
 
 export const FilmPage = () => {
+
   const { title } = useParams();
   const film = getFilmByTitle(title);
 
@@ -17,9 +18,38 @@ export const FilmPage = () => {
       release,
   } = film;
 
-  console.log(embedUrls);
+  let index = 0; 
 
   return (
-    <video src={embedUrls[2].url}/>
+    <>
+      <section className='film-page'>
+          <div className='row w-100'>
+              <div className='col'>
+                  <img className='film-page-image' src={image} alt={title} />
+              </div>
+              <div className='col'>
+                  <h3 style={{color:"white"}} >{title}</h3>
+                  <ul className='list-group list-group-flush'>
+                    <li className='list-group-item'>
+                      <span className='list-item-title'><b>Rating: </b>{rating}</span>
+                    </li>
+                    <li className='list-group-item'>
+                      <span className='list-item-title'><b>Release: </b>{release}</span>
+                    </li>
+                    <li className='list-group-item'><b>Genre: </b>{genres[0].name}</li>
+                    <li>
+                      <div className='col-10'>
+                        <h3 className='mt-2' style={{color:"white"}}>Description</h3>
+                        <p className='mt-2' style={{color:"white"}}>{description}</p>
+                      </div>
+                    </li>
+                  </ul>
+              </div>
+              <div className='col'>
+                  <iframe src={embedUrls[3].url} frameborder="0"></iframe>
+              </div>
+          </div>
+      </section>
+    </>
   )
 }
